dee6370fe01df2601e74ad545d662b7d0121ef0e
Lachezar Kutsarov <lkutsarov@gmail.com>
2026-02-20 01:19:37 +0100

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,24 @@
+
+function firstUniqueFreq(input: number[]): number {
+    const NO_UNIQUE_FREQUENCY = -1;
+    // inclusive range of possible values: [1, pow(10, 5)]
+    const maxValue = Math.max(...input);
+    const frequencyOfValues = new Array(maxValue + 1).fill(0);
+    const frequencyOfFrequencies = new Array(input.length + 1).fill(0);
+
+    for (let value of input) {
+        --frequencyOfFrequencies[frequencyOfValues[value]];
+        ++frequencyOfValues[value];
+        ++frequencyOfFrequencies[frequencyOfValues[value]];
+    }
+
+    let firstUniqueFrequency = NO_UNIQUE_FREQUENCY;
+    for (let value of input) {
+        if (frequencyOfFrequencies[frequencyOfValues[value]] === 1) {
+            firstUniqueFrequency = value;
+            break;
+        }
+    }
+
+    return firstUniqueFrequency;
+};
